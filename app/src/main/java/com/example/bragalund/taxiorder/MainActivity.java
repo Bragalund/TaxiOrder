0807dabc0675df46b46804645f0e91bc3df775e8
Bragalund <bakhen15@student.westerdals.no>
2017-05-05 20:16:50 +0200

    Har lagt til animasjon av tekst.

--- app/src/main/java/com/example/bragalund/taxiorder/MainActivity.java
+++ app/src/main/java/com/example/bragalund/taxiorder/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.bragalund.taxiorder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWidgets();
+        doAnimation();
     }
+
+    private void initWidgets() {
+        textview = (TextView) findViewById(R.id.firstTextView);
+    }
+
+    private void doAnimation() {
+        AnimatorClass animator = new AnimatorClass(textview, new String[]{
+                "We hope to see you again.",
+                "Order your taxi here!",
+                "Get it instantly.",
+                "The easy way to order a taxi."
+        });
+
+        animator.startAnimation();
+    }
+
+    public void startOrder() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/bragalund/taxiorder/MapActivity.java
@@ -0,0 +1,8 @@
+package com.example.bragalund.taxiorder;
+
+/**
+ * Created by bragalund on 05.05.17.
+ */
+
+public class MapActivity {
+}
