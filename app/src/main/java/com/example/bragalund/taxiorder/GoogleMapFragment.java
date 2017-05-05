32dc6266716065006c77979fe7f34b68f7b73450
Bragalund <bakhen15@student.westerdals.no>
2017-05-05 22:08:19 +0200

    Har lagt til en fragmentmanager i MapActivity-klassen. GoogleMapFragment-klassen er en ny klasse med all logikk for maps-fragmentet.

---
+++ app/src/main/java/com/example/bragalund/taxiorder/GoogleMapFragment.java
@@ -0,0 +1,35 @@
+package com.example.bragalund.taxiorder;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class GoogleMapFragment extends Fragment
+        implements OnMapReadyCallback {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragments_gmaps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        com.google.android.gms.maps.MapFragment fragment = (com.google.android.gms.maps.MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
--- app/src/main/java/com/example/bragalund/taxiorder/MapActivity.java
+++ app/src/main/java/com/example/bragalund/taxiorder/MapActivity.java
@@ -1,49 +1,20 @@
 package com.example.bragalund.taxiorder;
 
-
+import android.app.Activity;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.annotation.Nullable;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MapActivity extends FragmentActivity
-        implements OnMapReadyCallback {
-
-    private MapFragment mMapFragment;
-    private MapFragment mapFragment;
-    private GoogleMap map;
-
+public class MapActivity extends Activity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        initFragment();
-        onMapReady(map);
-    }
-
-    private void initFragment() {
-        mMapFragment = MapFragment.newInstance();
-        FragmentTransaction fragmentTransaction =
-                getFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.my_container, mMapFragment);
-        fragmentTransaction.commit();
-
-        mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.map, new GoogleMapFragment()).commit();
+        transaction.commit();
     }
 }
