527edb74ec757266fc583c03eb8df1bedf68e550
Bragalund <bakhen15@student.westerdals.no>
2017-05-05 20:20:42 +0200

    Har lagt til MapActivity-klassen til androidManifest.xml

--- app/src/main/java/com/example/bragalund/taxiorder/MapActivity.java
+++ app/src/main/java/com/example/bragalund/taxiorder/MapActivity.java
@@ -1,8 +1,9 @@
 package com.example.bragalund.taxiorder;
 
-/**
- * Created by bragalund on 05.05.17.
- */
 
-public class MapActivity {
+import android.support.v7.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity{
+
+
 }
